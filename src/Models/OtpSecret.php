0a3a9e8e2c02e4797b39bcdb0c442c86544375af
Kodnificent <mbamaravictor@gmail.com>
2023-06-04 21:13:43 +0100

    implement encryption

--- src/Models/OtpSecret.php
+++ src/Models/OtpSecret.php
@@ -2,14 +2,15 @@
 
 namespace Kodnificent\LaravelOtp\Models;
 use Illuminate\Database\Eloquent\Model;
+use Kodnificent\LaravelOtp\Models\Casts\Encrypted;
 
 class OtpSecret extends Model
 {
     protected $table = 'kodnificent_laravel_otp_otp_secrets';
 
     protected $casts = [
-        'secret_key' => 'encrypted',
-        'at' => 'encrypted',
+        'secret_key' => Encrypted::class,
+        'at' => Encrypted::class,
     ];
 
     protected $guarded = [];
